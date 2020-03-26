012e452b4856eda1272dda1cfb66dab4a42e38e3
MaksMasek <m.masek@zlotoryja.xyz>
2020-03-26 21:46:30 +0100

    praca nad selenium

---
+++ inbot.py
@@ -0,0 +1,26 @@
+import json
+from selenium import webdriver
+import datetime
+from time import sleep
+
+
+#def findword(pol):
+
+
+
+
+#def newword(ang):
+
+
+
+def logowanie():
+    login = 'majmas552'
+    password = 'tiemx'
+    browser = webdriver.Chrome('C:\\Users\\Maksymilian Masek\\Desktop\\chromedriver.exe')
+    browser.get('https://www.google.com/')
+    
+    #log = browser.
+    #haslo = browser.
+
+
+
