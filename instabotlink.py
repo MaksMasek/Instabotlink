ec1a8c11dacb1df024bb8e5b8457456dada6e801
MaksMasek <m.masek@zlotoryja.xyz>
2020-03-26 20:40:32 +0100

    dodany sam instatoblink.py

---
+++ instabotlink.py
@@ -0,0 +1,26 @@
+import json
+from selenium import webbrowser
+import datetime
+from time import sleep
+
+
+def findword(pol):
+
+
+
+
+def newword(ang):
+
+
+
+def logowanie():
+    login = 'majmas552'
+    password = 'tiemx'
+    link = 'https://instaling.pl/teacher.php?page=login'
+    browser = webbrowser.Chrome('C:\\Users\\Maksymilian Masek\\Desktop\\chromedriver.exe')
+    browser.get(link)
+    log = browser.find_element_by
+    haslo = browser.
+
+
+
